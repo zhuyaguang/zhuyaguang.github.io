aeb03677ee46455edd36235abc1cecd84fdb9ae4
zhuyaguang <zhuyaguang1368@163.com>
2022-07-10 15:10:05 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/zhuyaguang/.zprofile
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
 hugo --destination ./docs --buildDrafts --cleanDestinationDir 
 git add .
 git commit -m "update"
